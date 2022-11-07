7d3934802a344960af0dec064bfc9c6c386200f8
Carlos Montero Marín <46814503+CarlosMonteroMarin@users.noreply.github.com>
2022-11-07 09:08:38 +0100

    Primer commit con proyecto basico y estructura principal

---
+++ TA22-Patron-MVC/src/test/java/TA22_Patron_MVC/TA22_Patron_MVC/AppTest.java
@@ -0,0 +1,20 @@
+package TA22_Patron_MVC.TA22_Patron_MVC;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
